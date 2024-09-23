6d9732e9e0f3d12e0adc922c9edd6fad17e5c29a
Michael Pabst <mikeypbr3@gmail.com>
2024-09-22 21:45:13 -0400

    module 4 classwork

---
+++ Module 4/CEIS110.5.b.py
@@ -0,0 +1,12 @@
+#this function will display a message. The message is passed to the function as an argument
+def print_welcome(message):
+    print(message)
+    print()
+    
+def main():
+    message="Welcome to our program!"
+    print_welcome(message)
+    
+#The following code checks whether the current module is the main module
+if __name__ =="__main__":
+    main();
---
+++ Module 4/CEIS110.5.c.py
@@ -0,0 +1,17 @@
+
+'''
+Statements
+Variables
+Parameters
+Functions
+
+example:
+
+def my_function(name)
+    print(name="LaHire")
+
+    
+statement function(parameter)
+    statement(parameter=variable)
+
+'''    
