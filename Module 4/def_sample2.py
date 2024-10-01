c37c2250abc8df4aaf49ea3ec0b1f500b58a8a0b
Michael Pabst <mikeypbr3@gmail.com>
2024-10-01 14:57:02 -0400

    changed names

--- Module 4/def_sample2.py
+++ Module 4/def_sample2.py
@@ -1,4 +1,6 @@
-#this function will display a message. The message is passed to the function as an argument
+#this function will display a message. 
+
+# The message is passed to the function as an argument
 def print_welcome(message):
     print(message)
     print()
@@ -7,6 +9,7 @@ def main():
     message="Welcome to our program!"
     print_welcome(message)
     
-#The following code checks whether the current module is the main module
+#The following code checks whether the 
+# current module is the main module
 if __name__ =="__main__":
     main();
