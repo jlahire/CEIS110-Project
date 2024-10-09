2a7cb57996470bdf6a39abee66ce8fb122960781
Michael Pabst <mikeypbr3@gmail.com>
2024-10-09 17:42:32 -0400

    finished module 6

--- Module 6/ex_h_guess.py
+++ Module 6/ex_h_guess.py
@@ -1,7 +1,6 @@
 import random
 
-def numer_guess(num):
-    rand_num = random.seed(900)
+def number_guess(num):
     if num == rand_num:
         print('{} is correct!'.format(num))
     elif num < rand_num:
@@ -10,12 +9,13 @@ def numer_guess(num):
         print('{} is too high. Random number was {}.'.format(num,rand_num))
     
 if __name__ == "__main__":
+    
     random.seed(900)
-    
-    
+    rand_num = random.randint(1, 100)
     user_input = input()
+    
     tokens = user_input.split()
     for token in tokens:
         num = int(token)
-        numer_guess(num)
+        number_guess(num)   
     
